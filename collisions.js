315adc5573433b419ae5e122d7e84caf029d5522
Abdul Zakkar <mr.abdul51@gmail.com>
2019-05-22 18:56:17 -0500

    fixed hole launch bug (I think)

--- collisions.js
+++ collisions.js
@@ -217,10 +217,10 @@ function checkCircleHoleCollision(c, h)
 	if (dist < Math.pow(h.r * 1.5, 2))
 	{
 		var changeVelX = (h.x - c.x) * gravity * (1 / Math.pow(dist * 0.01, 2));
-		c.velX += changeVelX;
+		if (Math.abs(changeVelX) < 5.0) c.velX += changeVelX;
 
 		var changeVelY = (h.y - c.y) * gravity * (1 / Math.pow(dist * 0.01, 2));
-		c.velY += changeVelY;
+		if (Math.abs(changeVelY) < 5.0) c.velY += changeVelY;
 	}
 	var speed = Math.pow(c.velX, 2) + Math.pow(c.velY, 2)
 	if (dist < Math.pow(h.r * 0.25, 2))
